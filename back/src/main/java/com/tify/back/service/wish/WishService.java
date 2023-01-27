8e323b99b71da5221a2e0a4353546df8502cecc2
부울경_1반_강기한 <rkdrlgks321@naver.com>
2023-01-27 16:49:49 +0900

    S08P12E208-166

--- back/src/main/java/com/tify/back/service/wish/WishService.java
+++ back/src/main/java/com/tify/back/service/wish/WishService.java
@@ -2,16 +2,25 @@ package com.tify.back.service.wish;
 
 
 
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.tify.back.dto.wish.AddWishDto;
+import com.tify.back.model.gifthub.Gift;
 import com.tify.back.model.wish.Wish;
 import com.tify.back.repository.wish.WishRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class WishService {
     private final WishRepository wishRepository;
+    private final ObjectMapper objectMapper;
     // 위시 데이터 저장 서비스
     public Wish pureSave(Wish wish) {
         return wishRepository.save(wish);
@@ -19,9 +28,16 @@ public class WishService {
     public Wish findWishById(Long id) {
         return wishRepository.findById(id).orElse(null);
     }
+
+    // gift datas come in shape of JsonArray
     public boolean saveWish(AddWishDto dto) {
         Wish wishEntity = new Wish();
-        wishEntity.setGiftItems(dto.getGiftItem());
+        try {
+            List<Gift> giftItems = objectMapper.readValue((JsonParser) dto.getGiftItem(), new TypeReference<List<Gift>>(){});
+            wishEntity.setGiftItems(giftItems);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         wishEntity.setTotPrice(dto.getTotalPrice());
         wishEntity.setNowPrice(dto.getNowPrice());
         wishEntity.setTitle(dto.getWishTitle());
